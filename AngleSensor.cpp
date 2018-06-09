3ded0a5011565df0b6e0d6e3d7dc997e0cb62d4a
Erik Peschier <erik.peschier@purplefox.nl>
2018-06-09 23:51:59 +0200

    Added sensor templated class

--- AngleSensor.cpp
+++ AngleSensor.cpp
@@ -2,7 +2,7 @@
 
 #define FULL_ANGLE 300 // Full value of the rotary angle is 300 degrees.
 
-AngleSensor::AngleSensor(int pin, int voltage, unsigned int interval) : Task(interval) {
+AngleSensor::AngleSensor(int pin, int voltage, unsigned int interval) : Task(interval) , Sensor() {
   _inputPin = pin;
   _voltage = voltage;
 }
@@ -11,17 +11,8 @@ void AngleSensor::Init() {
   pinMode(_inputPin, INPUT);
 }
 
-float AngleSensor::GetValue() {
-  _previousValue = _degrees;
-  return _degrees;
-}
-
-bool AngleSensor::ValueChanged() {
-  return (_previousValue != _degrees);
-}
-
 void AngleSensor::Callback() {
   int sensor_value = analogRead(_inputPin);
   float voltage = (float)sensor_value * _voltage / 1023;
-  _degrees = (voltage*FULL_ANGLE) / _voltage;
+  _value = (voltage*FULL_ANGLE) / _voltage;
 }
